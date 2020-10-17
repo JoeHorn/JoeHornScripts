e97592ebdba4cce5ee39f4620373ef68550ba43c
Joe Horn <joehorn@Mainland.joehorn.idv.tw>
2020-10-18 00:57:36 +0800

    Old: Check if key changed, executed by cron job New: Certbot hooked scripts will be executed if any key was changed

--- letsencrypt-key-changed.sh
+++ letsencrypt-key-changed.sh
@@ -2,22 +2,28 @@
 
 PATH=/sbin:/usr/sbin:${PATH}
 
-KEY_DIR="/etc/letsencrypt/live"
-LOG_FILE="/tmp/letsencrypt-key-changed.log"
-CHANGED=""
 RESTART_SERVICES="nginx postfix"
 
-NOW=`date +%s`
-for FULL_CHAIN in ${KEY_DIR}/*/fullchain.pem; do
-	CTIME=`stat -c %Y ${FULL_CHAIN}`
-	DIFF=$(($NOW-$CTIME))
-	if [ $DIFF -lt 28800 ]; then
-		CHANGED=$NOW
-	fi
+# Old: Check if key changed, executed by cron job
+#KEY_DIR="/etc/letsencrypt/live"
+#LOG_FILE="/tmp/letsencrypt-key-changed.log"
+#CHANGED=""
+#NOW=`date +%s`
+#for FULL_CHAIN in ${KEY_DIR}/*/fullchain.pem; do
+#	CTIME=`stat -c %Y ${FULL_CHAIN}`
+#	DIFF=$(($NOW-$CTIME))
+#	if [ $DIFF -lt 28800 ]; then
+#		CHANGED=$NOW
+#	fi
+#done
+#if [ -n "$CHANGED" ]; then
+#	date >> ${LOG_FILE}
+#	for RESTART_SERVICE in $RESTART_SERVICES; do
+#		systemctl restart ${RESTART_SERVICE}
+#	done
+#fi
+
+# New: Certbot hooked scripts will be executed if any key was changed
+for RESTART_SERVICE in $RESTART_SERVICES; do
+	systemctl restart ${RESTART_SERVICE}
 done
-if [ -n "$CHANGED" ]; then
-	date >> ${LOG_FILE}
-	for RESTART_SERVICE in $RESTART_SERVICES; do
-		systemctl restart ${RESTART_SERVICE}
-	done
-fi
